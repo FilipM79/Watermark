8f7d952f9aaaf6efd55359b3d68e0115e3f35bb4
Filip Markovic <107422386+FilipM79@users.noreply.github.com>
2022-10-05 07:18:08 +0200

    Working phase 2/5 with OOP.

--- Watermark/task/src/watermark/Main.kt
+++ Watermark/task/src/watermark/Main.kt
@@ -4,83 +4,117 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.system.exitProcess
 
 fun main() {
 
-    lateinit var image: BufferedImage
-    lateinit var watermark: BufferedImage
-    var weight = 0
-    var outputName = ""
-    var extension = ""
+    val image = inputImage()
+    val watermark = inputWatermark(image)
+    val weight = inputWeight()
+    val outputName = outputImageFileName()
+    val extension = outputExtension(outputName)
+    val transparency = checkForTransparency(watermark)
+    val outputImage = outputImage(image, watermark, weight)
+    createOutputFile(outputName, extension, outputImage)
 
-    loop@ while (true) {
+}
 
-//        print("Input the image filename:\n> ")
-        val fileName = "image.png"
-//        val fileName = readln()
-        val imageFile = File(fileName)
-        if (!imageFile.exists() && !imageFile.isFile) {
-            println("The file $fileName doesn't exist.")
-            break@loop
-        } else if (ImageIO.read(imageFile).colorModel.pixelSize !in 24..32) {
-            println("The image isn't 24 or 32 bit.")
-            break@loop
-        } else if (ImageIO.read(imageFile).colorModel.numComponents != 3) {
-            println("The number of image color components isn't 3.")
-            break@loop
-        }
+fun inputImage(): BufferedImage {
+    val image: BufferedImage
+    print("Input the image filename:\n> ")
+    val imageName = readln()
+    val imageFile = File(imageName)
+
+    if (!imageFile.exists() && !imageFile.isFile) {
+        println("The file $imageName doesn't exist.")
+        exitProcess(0)
+    } else if (ImageIO.read(imageFile).colorModel.numComponents !in 3..4) {
+        println("The number of image color components isn't 3 or 4.")
+        exitProcess(0)
+    } else if (ImageIO.read(imageFile).colorModel.pixelSize !in 24..32) {
+        println("The image isn't 24 or 32-bit.")
+        exitProcess(0)
+    } else {
         image = ImageIO.read(imageFile)
+    }
+    return image
+}
+fun inputWatermark(image: BufferedImage): BufferedImage {
+    val watermark: BufferedImage
+    print("Input the watermark image filename:\n> ")
+    val watermarkName = readln()
+    val watermarkFile = File(watermarkName)
 
-//        print("Input the watermark image filename:\n> ")
-        val watermarkInput = "watermark.png"
-//        val watermarkInput = readln()
-        val watermarkFile = File(watermarkInput)
-        watermark = if (!watermarkFile.exists() && !watermarkFile.isFile) {
-            println("The file $fileName doesn't exist.")
-            break@loop
-        } else if (ImageIO.read(imageFile).colorModel.pixelSize !in 24..32) {
-            println("The image isn't 24 or 32 bit.")
-            break@loop
-        } else if ((image.height != ImageIO.read(imageFile).height) && (image.width != ImageIO.read(imageFile).width)) {
-            println("The image and watermark dimensions are different.")
-            break@loop
-        } else {
-            ImageIO.read(imageFile)
+    if (!watermarkFile.exists() && !watermarkFile.isFile) {
+        println("The file $watermarkName doesn't exist.")
+        exitProcess(0)
+    } else if (ImageIO.read(watermarkFile).colorModel.numComponents !in 3..4) {
+        println("The number of watermark color components isn't 3 or 4.")
+        exitProcess(0)
+    } else if (ImageIO.read(watermarkFile).colorModel.pixelSize !in 24..32) {
+        println("The watermark isn't 24 or 32-bit.")
+        exitProcess(0)
+    } else if ((image.height != ImageIO.read(watermarkFile).height) && (image.width != ImageIO.read(watermarkFile).width)) {
+        println("The image and watermark dimensions are different.")
+        exitProcess(0)
+    } else {
+        watermark = ImageIO.read(watermarkFile)
+    }
+    return watermark
+}
+fun inputWeight (): Int {
+    val weight: Int
+
+    print("Input the watermark transparency percentage (Integer 0-100):\n> ")
+    val weightString = readln()
+
+    for (char in weightString) {
+        if (!char.isDigit()) {
+            println("The transparency percentage isn't an integer number.")
+            exitProcess(0)
         }
-
-//        print("Input the watermark transparency percentage (Integer 0-100):\n> ")
-        val weightString = "50"
-//        val weightString = readln()
-        for (char in weightString) {
-            if (!char.isDigit()) {
-                println("The transparency percentage isn't an integer number.")
-                break@loop
-            }
-        }
-        weight = weightString.toInt()
-        if (weight !in 0..100) {
-            println("The transparency percentage is out of range.")
-            break@loop
-        }
-
-//        print("Input the output image filename (jpg or png extension):\n> ")
-        outputName = "test.jpg"
-//        outputName = readln()
-        val png = outputName.endsWith("png")
-        val jpg = outputName.endsWith("jpg")
-
-        if (!png && !jpg) {
-            println("The output file extension isn't \"jpg\" or \"png\".")
-            break@loop
-        }
-        extension = if (png) ".png" else ".jpg"
-
-        break@loop
     }
 
+    weight = weightString.toInt()
+
+    if (weight !in 0..255) {
+        println("The transparency percentage is out of range.")
+        exitProcess(0)
+    }
+    return weight
+}
+fun outputImageFileName (): String {
+
+    print("Input the output image filename (jpg or png extension):\n> ")
+    val outputName = readln()
+
+    val png = outputName.endsWith("png")
+    val jpg = outputName.endsWith("jpg")
+
+    if (!png && !jpg) {
+        println("The output file extension isn't \"jpg\" or \"png\".")
+        exitProcess(0)
+    }
+    return outputName
+}
+fun outputExtension(outputName: String): String {
+    return if (outputName.endsWith("png")) "png" else "jpg"
+}
+fun checkForTransparency (watermark: BufferedImage): Boolean {
+    return if (watermark.transparency == 3) {
+        println("Do you want to use the watermark's Alpha channel?")
+//        if (readln() == "yes") {
+//            ...
+//        }
+        true
+    } else false
+}
+fun outputImage (image: BufferedImage, watermark: BufferedImage, weight: Int): BufferedImage {
     val outputImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+
     for (x in 0 until image.width) {
         for (y in 0 until image.height) {
+
             val i = Color(image.getRGB(x, y))
             val w = Color(watermark.getRGB(x, y))
 
@@ -92,55 +126,13 @@ fun main() {
             outputImage.setRGB(x, y, color.rgb)
         }
     }
-
+    return outputImage
+}
+fun createOutputFile (outputName: String, extension: String, outputImage: BufferedImage): File {
     val outputFile = File(outputName)
-    outputFile.resolve(outputName)
     outputFile.createNewFile()
     ImageIO.write(outputImage, extension, outputFile)
 
-    println(outputFile.absolutePath)
-    println(outputName)
-    println(outputFile.isFile)
+    println("The watermarked image $outputName has been created.")
+    return outputFile
 }
-
-
-
-//    if (imageFile.isFile) {
-//        val image: BufferedImage = ImageIO.read(imageFile)
-//        var transparency = ""
-//
-//        when (image.colorModel.transparency) {
-//            1 -> transparency = "OPAQUE"
-//            2 -> transparency = "BITMASK"
-//            3 -> transparency = "TRANSLUCENT"
-//        }
-//
-//        println("Image file: $imageFile")
-//        println("Width: ${image.width}")
-//        println("Height: ${image.height}")
-//        println("Number of components: ${image.colorModel.numComponents}")
-//        println("Number of color components: ${image.colorModel.numColorComponents}")
-//        println("Bits per pixel: ${image.colorModel.pixelSize}")
-//        println("Transparency: $transparency")
-//    } else {
-//        println("The file $imageFile doesn't exist.")
-//    }
-
-//    // Finding the directory in the File with the biggest number of files in it.
-//    val filesDirectory = File("C:\\Users\\Filip\\Downloads\\basedir\\basedir")
-//    val listOfFiles = filesDirectory.walkTopDown().toList()
-//    var filesInDir = 0
-//    var biggestDir = ""
-//
-//    for (file in listOfFiles) {
-//        if (file.isDirectory && file.name != "basedir") {
-//            if ((file.listFiles()?.size ?: 0) > filesInDir) {
-//                filesInDir = file.listFiles()?.size ?: 0
-//                biggestDir = file.name
-//            }
-//        }
-//    }
-//
-//    println("$biggestDir $filesInDir")
-
-//}
